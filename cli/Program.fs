4e11769580091dd8cad452d6e47a393bbd70384d
Pape Barka <papebarka@gmail.com>
2024-07-27 00:50:59 +0000

    Adds License typing

--- cli/Program.fs
+++ cli/Program.fs
@@ -1,2 +1,21 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
+﻿open System
+
+type Name = Name of string
+type Department = Department of string
+type Periodicity = Monthly | Yearly
+type StartDate = StartDate of Date
+type Cost = Cost of int
+
+type LicenseId = LicenseId of int
+
+type License =
+    {
+        Id : LicenseId
+        Name : Name
+        Department : Department
+        Billing : Periodicity
+        StartedOn : StartDate
+        Price : Cost
+    }
+// For more information see https://aka.ms/fsharp-console-apps
 printfn "Hello from F#"
