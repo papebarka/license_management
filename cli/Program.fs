dc2101a600f45a9acd0e13a6e73ac259768d69c2
Pape Barka <papebarka@gmail.com>
2024-07-27 16:32:37 +0000

    Adding software types

--- cli/Program.fs
+++ cli/Program.fs
@@ -3,7 +3,7 @@
 type Name = Name of string
 type Department = Department of string
 type Periodicity = Monthly | Yearly
-type StartDate = StartDate of Date
+type StartDate = StartDate of DateOnly
 type Cost = Cost of int
 
 type LicenseId = LicenseId of int
@@ -17,5 +17,20 @@ type License =
         StartedOn : StartDate
         Price : Cost
     }
+
+
+type SubscribedSoftwares = SubscribedSoftwares of ResizeArray<_>
+
+let adobe : License =
+    {
+        Id = 10001
+        Name = "Adobe CC"
+        Department = "MCC"
+        Billing = Yearly
+        StartedOn = (2024, 08, 30)
+        Price = 1200000
+    }
+
+
 // For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+printfn $"Software product: {adobe}"
